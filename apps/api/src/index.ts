025c6de9dbec9f2f3d3e19f6ec14ecfa0997b137
Thomaz Takashi Oda Toyama <thomazot@gmail.com>
2024-08-06 17:45:19 -0300

    feat: model and permission casl

---
+++ apps/api/src/index.ts
@@ -0,0 +1,14 @@
+import { defineAbilityFor } from "@saas/auth";
+
+const ability = defineAbilityFor({ role: 'ADMIN' })
+
+const userCanInviteSomeoneElse = ability.can('invite', 'User')
+const userCanDeleteOtherUsers = ability.can('delete', 'User')
+
+const userCannotDeleteOtherUsers = ability.cannot('delete', 'User')
+
+console.log({
+    userCanInviteSomeoneElse,
+    userCanDeleteOtherUsers,
+    userCannotDeleteOtherUsers
+})
