69250eae9608eb6e80b57759922f7a7553e9be36
Thomaz Takashi Oda Toyama <thomazot@gmail.com>
2024-08-07 09:08:38 -0300

    feat: add zod and subjects

--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -2,8 +2,7 @@ import { AbilityBuilder } from '@casl/ability'
 
 import { AppAbility } from '.'
 import { User } from './models/user'
-
-type Role = 'ADMIN' | 'MEMBER'
+import { Role } from './roles'
 
 type PermissionsByRole = (
   user: User,
@@ -16,5 +15,7 @@ export const permissions: Record<Role, PermissionsByRole> = {
   },
   MEMBER(_, { can }) {
     can('invite', 'User')
+    can('manage', 'Project')
   },
+  BILLING() {},
 }
---
+++ packages/auth/src/subjects/user.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod'
+
+export const userSubject = z.tuple([
+  z.union([
+    z.literal('invite'),
+    z.literal('delete'),
+    z.literal('manage'),
+    z.literal('get'),
+    z.literal('update'),
+  ]),
+  z.literal('User'),
+])
+
+export type UserSubject = z.infer<typeof userSubject>
